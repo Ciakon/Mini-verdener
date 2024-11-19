520bea442899af6c9a15b45894cf872af3866919
Ciakon <noahkarottki@gmail.com>
2024-11-19 15:22:12 +0100

    intelliJ L

--- code/src/main.java
+++ code/src/main.java
@@ -1,5 +1,3 @@
-package src;
-
 import itumulator.executable.Program;
 import itumulator.world.World;
 
@@ -7,7 +5,7 @@ class main {
     public static void main(String[] args) {
         System.out.println("L");
 
-        Program program = new Program(10, 900, 200);
+        Program program = new Program(10, 700, 200);
         World world = program.getWorld();
 
         program.show();
