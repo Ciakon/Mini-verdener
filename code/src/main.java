e44fefe93d54ad64987d9bcb960130abcef18d81
Ciakon <noahkarottki@gmail.com>
2024-11-19 15:09:50 +0100

    setup fix

--- code/src/main.java
+++ code/src/main.java
@@ -1,5 +1,13 @@
+import itumulator.executable.Program;
+import itumulator.world.World;
+
 class main {
     public static void main(String[] args) {
         System.out.println("L");
+
+        Program program = new Program(10, 900, 200);
+        World world = program.getWorld();
+
+        program.show();
     }
 }
