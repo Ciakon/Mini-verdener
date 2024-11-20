4039f23686deab21fbeb0fdef4191609679cf7b0
Hassan Al Wakiel <halw@itu.dk>
2024-11-20 15:56:32 +0100

    Update Rabbit.java
    Rabbit movement update

--- code/src/Rabbit.java
+++ code/src/Rabbit.java
@@ -1,21 +1,156 @@
-import java.awt.Color;
 
 import itumulator.executable.DisplayInformation;
 import itumulator.executable.DynamicDisplayInformationProvider;
 import itumulator.simulator.Actor;
+import itumulator.world.Location;
 import itumulator.world.World;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
-public class Rabbit implements Actor, DynamicDisplayInformationProvider{
-    
+public class Rabbit implements Actor, DynamicDisplayInformationProvider {
+
+    boolean isAlive;
+    boolean hasEaten;
+    int age;
+    String imageKey;
+    int visionRange;
+    boolean canBreed;
+
     public Rabbit() {
-
+        this.isAlive = true;
+        this.hasEaten = false;
+        this.age = 0;
+        this.imageKey = "rabbit-small";
+        this.visionRange = 3;
+        this.canBreed = false;
     }
 
     public void act(World world) {
-        
+        if (world.isNight() && !this.hasEaten) {
+            this.isAlive = false;
+        }
+        if (!this.isAlive) {
+            world.remove(this);
+        }
+        if (age == 6) {
+            makeOld();
+        }
+        movementAI(world);
     }
 
     public DisplayInformation getInformation() {
-        return new DisplayInformation(Color.blue, "rabbit-large");
+        return new DisplayInformation(Color.blue, this.imageKey);
+    }
+
+    public void makeOld() {
+        this.imageKey = "rabbit-large";
+        this.visionRange = 2;
+        this.canBreed = true;
+    }
+
+    /**
+     * Finds nearby grass, that is not accoupied by other rabbits
+     *
+     * @param worldworld which the rabbit is in
+     * @return Arraylist of all grass locations seen nearby
+     */
+    public ArrayList<Location> findGrass(World world) {
+        ArrayList<Location> nearbyGrass = new ArrayList<>();
+        Set<Location> surroundings = world.getSurroundingTiles(world.getLocation(this), this.visionRange); //Set of surrounding tiles within visionRange
+        for (Location location : surroundings) {
+            if (!world.isTileEmpty(location)) {
+                if (world.getTile(location) instanceof Grass) { //will not be seen if rabbit is currently on tile
+                    nearbyGrass.add(location);
+                }
+            }
+        }
+        return nearbyGrass;
+    }
+
+    /**
+     * Finds nearest grass that is unoccupied
+     *
+     * @param world world which the rabbit is in
+     * @param Arraylist Arraylist of all grass locations seen nearby
+     */
+    public Location nearestGrass(World world, ArrayList<Location> grass) {
+        Location closest = grass.get(0);
+        int highestValue;
+        if (closest.getX() > closest.getY()) {
+            highestValue = closest.getX();
+        } else {
+            highestValue = closest.getY();
+        }
+        for (Location location : grass) {
+            if (location.getX() < highestValue && location.getY() < highestValue) {
+                closest = location;
+                if (location.getX() > location.getY()) {
+                    highestValue = location.getX();
+                } else {
+                    highestValue = location.getY();
+                }
+            }
+        }
+        return closest;
+    }
+
+    /**
+     * Moves 1 tile towards a given location
+     *
+     * @param world world which the rabbit is in
+     * @param desiredlocation location to move towards
+     */
+    public void moveTowards(World world, Location desiredLocation) {
+        int movementInX = world.getLocation(this).getX();
+        int movementInY = world.getLocation(this).getY();
+        if (world.getLocation(this).getX() < desiredLocation.getX()) {
+            movementInX++;
+        } else if (world.getLocation(this).getX() > desiredLocation.getX()) {
+            movementInX--;
+        }
+
+        if (world.getLocation(this).getY() < desiredLocation.getY()) {
+            movementInY++;
+        } else if (world.getLocation(this).getY() > desiredLocation.getY()) {
+            movementInY--;
+        }
+        Location movement = new Location(movementInX, movementInY);
+        world.move(this, movement);
+    }
+
+    /**
+     * chooses random location out of set of locations
+     *
+     * @param locationSet a Set<> of Location
+     */
+    public Location randomLocation(Set<Location> LocationSet) {
+        if (LocationSet.isEmpty()) {
+            return null;
+        }
+        List<Location> LocationlList = new ArrayList<>(LocationSet);
+        Random random = new Random();
+        int randomIndex = random.nextInt(LocationlList.size());
+        return LocationlList.get(randomIndex);
+    }
+
+    /**
+     * Moves rabbit in direction of grass if seen otherwise in a random
+     * direction
+     *
+     * @param world
+     */
+    public void movementAI(World world) {
+        ArrayList<Location> nearbyGrass = findGrass(world);
+        if (!nearbyGrass.isEmpty()) {
+            Location desiredGrass = nearestGrass(world, nearbyGrass);
+            moveTowards(world, desiredGrass);
+        } else {
+            Set<Location> freeLocations = world.getEmptySurroundingTiles(world.getLocation(this));
+            Location nextLocation = randomLocation(freeLocations);
+            world.move(this, nextLocation);
+        }
     }
 }
