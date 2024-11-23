9c204948ab1ae846e3dddef4bba8ca970b416612
Hassan Al Wakiel <halw@itu.dk>
2024-11-23 18:01:38 +0100

    quick fix

--- code/src/Rabbit.java
+++ code/src/Rabbit.java
@@ -43,6 +43,7 @@ public class Rabbit implements Actor, DynamicDisplayInformationProvider {
             world.delete(this);
             return;
         }
+        DayTimeMovementAI(world);
     }
 
     public DisplayInformation getInformation() {
