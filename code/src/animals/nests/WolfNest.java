e26ad30d3830b0f7dc77f2be244f443c7dafd43a
Ciakon <118453795+Ciakon@users.noreply.github.com>
2024-12-11 19:39:24 +0100

    removed wolves from wolfnest

--- code/src/animals/nests/WolfNest.java
+++ code/src/animals/nests/WolfNest.java
@@ -1,17 +1,11 @@
 package animals.nests;
 
-import animals.Wolf;
 import itumulator.world.Location;
 import itumulator.world.World;
-import java.util.ArrayList;
 
 public class WolfNest extends AnimalNest {
-
-    ArrayList<Wolf> wolves;
-
     public WolfNest(World world, Location location) {
         super(world, location);
-        this.wolves = new ArrayList<>();
     }
 
 }
